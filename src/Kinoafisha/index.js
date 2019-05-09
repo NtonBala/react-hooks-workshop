78b90da28d82ba915246077b397a27259caf428d
Anton Balashov <anton.b@coaxsoft.com>
2019-05-09 14:37:06 +0300

    implemented hometask1 - restored footer styles

--- src/Kinoafisha/index.js
+++ src/Kinoafisha/index.js
@@ -47,7 +47,8 @@ export const Kinoafisha = () => {
     );
   });
 
-  return <>
+  return (
+    <>
       <div className="header">
         <div className="logo"/>
 
@@ -78,8 +79,18 @@ export const Kinoafisha = () => {
         </div>
       </div>
 
-    <div className="content">
-      {moviesJSX}
-    </div>
+      <div className="content">
+        {moviesJSX}
+      </div>
+
+      <div className="footer">
+        <a href="mailto:team@lectrum.io">team@lectrum.io</a>
+        <span>2019 © Все права защищены. Разработано с любовью в&nbsp; <a href="https://lectrum.io/intensive/react" rel="noreferrer noopener" target="_blank">Лектруме</a>.</span>
+        <div className="social">
+          <a className="facebook" href="https://www.facebook.com/lectrum/" rel="noreferrer noopener" target="_blank"/>
+          <a className="telegram" href="https://t.me/lectrum" rel="noreferrer noopener" target="_blank"/>
+        </div>
+      </div>
     </>
+  );
 };
